9d1de2afc471b2824e4298257bc675e64166e460
bethanyann <bethanyann44@gmail.com>
2023-04-17 22:46:40 -0500

    implement crystal balls algo

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    //get the square root of the length of the array so we know how big the jumps are
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    //use the first crystal ball to see where it breaks
+    let i = jumpAmount;
+    for(; i < breaks.length; i += jumpAmount) {
+        if(breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+    for(let j = 0; j <= jumpAmount && i < breaks.length; ++j , ++i) {
+        if(breaks[i]){
+            return i;
+        }
+    }
+
+    return -1;
 }
